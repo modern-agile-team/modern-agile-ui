423c4741b341b2b414d7f47acfd7733f206dc462
Min <alstnsrl98@gmail.com>
2022-02-07 23:21:14 +0900

    Feat: 컨셉 색상 파일 생성

---
+++ src/shared/theme.ts
@@ -0,0 +1,51 @@
+import { ColorTypes } from "./types/colors";
+
+export const COLORS: ColorTypes = {
+  MAIN: {
+    COMMON: "#6550FF",
+    DARKER: "#1324CB",
+    LIGHTER: "#9F7EFF",
+    SUBTLE: "#ECE5FF",
+  },
+  ERROR: {
+    COMMON: "#FF3B3B",
+    DARKER: "#E63535",
+    LIGHTER: "#FF5C5C",
+    SUBTLE: "#FF8080",
+  },
+  WARNING: {
+    COMMON: "#FFCC00",
+    DARKER: "#E6B800",
+    LIGHTER: "#FDDD48",
+    SUBTLE: "#FDED72",
+  },
+  INFO: {
+    COMMON: "#0063F7",
+    DARKER: "#004FC4",
+    LIGHTER: "#5B8DEF",
+    SUBTLE: "#9DBFF9",
+  },
+  SUCCESS: {
+    COMMON: "#06C270",
+    DARKER: "#05A660",
+    LIGHTER: "#39D98A",
+    SUBTLE: "#57EBA1",
+  },
+  DARK: {
+    DARK1: "#1E1E1E",
+    DARK2: "#535962",
+    DARK3: "#828B99",
+    DARK4: "#A9B4C3",
+  },
+  LIGHT: {
+    LIGHT1: "#CED7E1",
+    LIGHT2: "#DBE1E9",
+    LIGHT3: "#ECF0F5",
+    LIGHT4: "#F9FBFF",
+  },
+  LINEAR: {
+    LINEAR1: "background: linear-gradient(180deg, #6550FF 0%, rgba(101, 80, 255, 0) 85.61%);",
+    LINEAR2:
+      "background: linear-gradient(180deg, rgba(236, 229, 255, 0.5) 0%, rgba(236, 229, 255, 0) 100%);",
+  },
+};
