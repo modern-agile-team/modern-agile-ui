2197a52e3a8d87b254856036dd795e1e5541571b
Min <alstnsrl98@gmail.com>
2022-02-09 21:48:21 +0900

    Feat(radio): 클릭시 checkCircle 표시되도록 구현 #6

--- src/components/MARadio/index.tsx
+++ src/components/MARadio/index.tsx
@@ -1,12 +1,17 @@
+import { useCallback, useState } from "react";
 import { CheckCircle, InvisibleRadio, StyledRadioLabel, VisibleRadio } from "./style";
 
 const MARadio = () => {
+  const [isCheck, setIsCheck] = useState<boolean>(false);
+
+  const clickRadio = useCallback(() => {
+    setIsCheck(!isCheck);
+  }, [isCheck]);
+
   return (
     <StyledRadioLabel>
-      <InvisibleRadio type="radio" />
-      <VisibleRadio>
-        <CheckCircle />
-      </VisibleRadio>
+      <InvisibleRadio type="radio" onChange={clickRadio} />
+      <VisibleRadio>{isCheck && <CheckCircle />}</VisibleRadio>
     </StyledRadioLabel>
   );
 };
